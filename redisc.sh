1d9df164b484f3dce10bd257e458450b6db6d3e5
vinay davin <vinaydavin@gmail.com>
2026-01-14 13:46:55 +0530

    3m

--- redisc.sh
+++ redisc.sh
@@ -6,8 +6,10 @@ dnf remove redis\* -y &>> ${log_file}
 validate $? "Disabling Old Redis Module"
 dnf install epel-release -y && dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
  &>> ${log_file}
+sudo dnf module reset redis -y
+sudo dnf module enable redis:remi-7.2 -y &>> ${log_file}
 validate $? "Enabling Redis 7 Module"
-dnf module enable redis:7/remi -y && dnf install redis -y &>> ${log_file}
+dnf install redis -y &>> ${log_file}
 validate $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>> ${log_file}

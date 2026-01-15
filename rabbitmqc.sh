e1a2b4348b0149d9c9481d63c819cfa90655eb23
vinay davin <vinaydavin@gmail.com>
2026-01-15 15:32:42 +0530

    3m

--- rabbitmqc.sh
+++ rabbitmqc.sh
@@ -4,7 +4,12 @@ source ./common.sh
 check_root
 $app_name=rabbitmq
 app_setup
-systemd_setup
-app_restart
-
+cp $script_dir/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>> ${log_file}
+dnf install rabbitmq-server -y &>> ${log_file}
+validate $? "Installing RabbitMQ Server"
+systemctl enable rabbitmq-server &>> ${log_file}
+validate $? "Enabling RabbitMQ Service" 
+systemctl start rabbitmq-server &>> ${log_file}
+validate $? "Starting RabbitMQ Service"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 print_total_time
